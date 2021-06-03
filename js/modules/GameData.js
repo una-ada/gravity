aacc88536281af9d9c7b910d8d67629a29096bfb
unhoglund <trewbot@gmail.com>
2021-06-03 10:02:31 -0700

    Add GameData#scene

--- js/modules/GameData.js
+++ js/modules/GameData.js
@@ -6,22 +6,25 @@
 
 /*----- Imports --------------------------------------------------------------*/
 import { Point, Vector } from "./Utils.js";
+import Celestial from "./Celestial.js";
 
 /*----- Classes --------------------------------------------------------------*/
-/** @module GameData - Manages the game model. */
+/** @module GameData Manages the game model. */
 export default class {
   /** Initialize a game model. */
   constructor() {
-    /** @var {string} id - Short game ID based on the current time. */
+    /** @var {string} id Short game ID based on the current time. */
     this.id = Math.round(+new Date() / 1e3).toString(26);
-    /** @var {Object} mouse - Data regarding the user's mouse. */
+    /** @var {Object} mouse Data regarding the user's mouse. */
     this.mouse = {
-      /** @var {boolean} mouse.isDown - Is mouse currently down? */
+      /** @var {boolean} mouse.isDown Is mouse currently down? */
       isDown: false,
-      /** @var {Object} mouse.initPosition - Position of mouse on down. */
+      /** @var {Object} mouse.initPosition Position of mouse on down. */
       initPosition: new Point(0, 0),
-      /** @var {Object} mouse.position - Current position of mouse. */
+      /** @var {Object} mouse.position Current position of mouse. */
       position: new Point(0, 0),
     };
+    /** @var {Celestial[]} scene Array of game objects in current scene. */
+    this.scene = [];
   }
 }
